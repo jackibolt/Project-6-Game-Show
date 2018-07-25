93139536e70b66ed48d5a19a306791cf3f2936ff
Jacki Bolt <bolt.jacqueline@gmail.com>
2018-07-24 22:49:59 -0400

    more progress

--- app.js
+++ app.js
@@ -57,23 +57,38 @@ console.log(characters)
 // add characters to display
 addPhraseToDisplay(characters);
 
+// let matched = null;
+
 // check if selected letter matches a character in the phrase
 const checkLetter = (e) => {
   const selected = e.target;
   const letter = document.querySelectorAll('.letter');
-  let matched = false;
+  // let matched = null;
   for (i=0; i<letter.length; i+=1) {
     if (selected.textContent === letter[i].textContent) {
       letter[i].classList.add('show');
       const match = letter[i].textContent;
       let matched = true;
-      console.log(matched);
-    // } else if (selected.textContent !== letter[i].textContent){
-    //   let matched = null;
+      // console.log(matched);
+    } else {
+      let matched = null;
     }
   }
+  // console.log(matched);
   return matched;
-  console.log(matched);
 }
-// run checkLetter function
-qwerty.addEventListener('click', checkLetter);
+
+qwerty.addEventListener('click', (e) => {
+      // disable chosen buttons
+  if (e.target.tagName === 'BUTTON') {
+    e.target.classList.add('chosen');
+    e.target.setAttribute('disabled', 'true');
+  }
+      // run checkLetter function
+  checkLetter(e);
+      // add to scoreBoard
+  if (matched === null) {
+    const scoreBoard = document.querySelector('#scoreboard');
+    console.log(scoreBoard);
+  }
+});
